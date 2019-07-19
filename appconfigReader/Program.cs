b55825dad785ae8ffd0076c214edf4407228e692
Knar <knarxr@protonmail.com>
2019-07-18 22:27:07 -0500

    Successfully parses appsettings key/values

--- appconfigReader/Program.cs
+++ appconfigReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace appconfigReader
 {
@@ -7,19 +8,30 @@ namespace appconfigReader
     {
         static void Main(string[] args)
         {
-            var file = "path/to/config/file/web.config";
+            var file = Console.ReadLine();
             var parsersToUse = new List<IConfigurationParser> {
                 new KeyValueParser(),
                 new KeyValueParser("name", "connectionString")
             };
 
-            // create the provider
             var provider = new ConfigFileConfigurationProvider(
                 file, loadFromFile: true, optional: false, null, parsersToUse);
 
-            // Read and parse the file
             provider.Load();
+            const string SectionDelimiter = "_";
+            var keyValues = provider.GetChildKeys(Enumerable.Empty<string>(), null)
+                .Select(key =>
+                {
+                    provider.TryGet(key, out var value);
+                    var newKey = string.IsNullOrEmpty(SectionDelimiter) ? key : key.Replace(SectionDelimiter, ":", StringComparison.OrdinalIgnoreCase);
+                    return new KeyValuePair<string, string>(newKey, value);
+                });
+            foreach (var key in keyValues)
+            {
+                Console.WriteLine($"{key.Key}, {key.Value}");
+            }
 
+            Console.ReadLine();
         }
     }
 }
